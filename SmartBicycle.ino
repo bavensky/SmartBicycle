0e87c8125cbd971bf041c2be568d27b252dfbec5
aphirak sang-ngenchai <apiruk_sang-ngenchai@hotmail.com>
2016-01-26 22:38:28 +0700

    no message

--- SmartBicycle.ino
+++ SmartBicycle.ino
@@ -1,22 +1,30 @@
 /***************************************************
   This is a library for 1.8 SPI TFT 128*160 display.
   link more :
+  http://lucidtronix.com/tutorials/13
+  https://www.youtube.com/watch?v=O5YYsm_BqJQ
   http://blog.simtronyx.de/en/a-1-8-inch-tft-color-display-hy-1-8-spi-and-an-arduino/
   http://www.tweaking4all.com/hardware/arduino/sainsmart-arduino-color-display/
 
-  pin connect :
-  Arduino   <----------> HY-1.8 SPI   or  SD Card
-  D4        <---------------------------> Pin 14 (CS)
-  D9        <----------> Pin 07 (A0)
-  D10(SS)   <----------> Pin 10 (CS)
-  D11(MOSI) <----------> Pin 08 (SDA)---> Pin 12 (MOSI)
-  D12(MISO) <---------------------------> Pin 13 (MISO)
-  D13(SCK)  <----------> Pin 09 (SCK)---> Pin 11 (SCK)
-  D8        <----------> Pin 06 (RESET)
-  5V(VCC)   <----------> Pin 02 (VCC)
-  GND       <----------> Pin 01 (GND)
-  5V(VCC)   <----------> Pin 15 (LED+)
-  GND       <----------> Pin 16 (LED-)
+  Device :
+    - Arduino
+    - 1.8 SPI TFT 128*160 display
+    - SS1305 Unipolar Hall Effect Sensor
+
+
+  Pin connect :
+    Arduino   <----------> HY-1.8 SPI   or  SD Card
+    D4        <---------------------------> Pin 14 (CS)
+    D9        <----------> Pin 07 (A0)
+    D10(SS)   <----------> Pin 10 (CS)
+    D11(MOSI) <----------> Pin 08 (SDA)---> Pin 12 (MOSI)
+    D12(MISO) <---------------------------> Pin 13 (MISO)
+    D13(SCK)  <----------> Pin 09 (SCK)---> Pin 11 (SCK)
+    D8        <----------> Pin 06 (RESET)
+    5V(VCC)   <----------> Pin 02 (VCC)
+    GND       <----------> Pin 01 (GND)
+    5V(VCC)   <----------> Pin 15 (LED+)
+    GND       <----------> Pin 16 (LED-)
  ****************************************************/
 
 #include <Adafruit_GFX.h>    // Core graphics library
@@ -31,10 +39,43 @@
 
 Adafruit_ST7735 tft = Adafruit_ST7735(TFT_CS, TFT_DC, TFT_RST);
 
+/********************* Initial speedometer ***************************/
+String message = "Luf";
+int mode = 1;
+int num_modes = 2;
+int cur_scroll = 0;
+int cur_msg_length = 0;
+int cursor_index = 0;
+int char_index = 0;
+int btn = 3;
+int hall_pin = 16;
+byte backlight_pwm = 200;
+int hall_state = 1;
+int revolutions = 0;
+float miles_in_inches = 63360;
+float kmh = 0.0, pev_kmh = 0.0;
+float mph = 0.0, pev_mph = 0.0;
+float distance = 0.0, pev_distance = 0.0;
+float k_ipm_2_mph;
+int tire_circumference;
+unsigned long last_fall = 0;
+unsigned long last_interval = 0;
+int cur_x = 8;
+int cur_y = 1;
+float tire_diameter = 26; // 700X23C = 26 Inch x 1.25 Inch  
+/*********************************************************************/
+/************************* Clear Screen ******************************/
+unsigned long previousMillis = 0;
+const long interval = 1000;
+
 void setup(void) {
   Serial.begin(9600);
 
   tft.initR(INITR_BLACKTAB);
+
+  k_ipm_2_mph = 3600000 / miles_in_inches;
+  tire_circumference = tire_diameter * 3.14159;
+  pinMode(hall_pin, INPUT);
 
   Serial.print("Initializing SD card...");
   if (!SD.begin(SD_CS)) {
@@ -43,22 +84,75 @@ void setup(void) {
   }
   Serial.println("OK!");
   tft.setRotation(3);
-  bmpDraw("man.bmp", 0, 0);
-  delay(3000);
+//  bmpDraw("man.bmp", 0, 0);
+//  delay(1000);
   tft.fillScreen(ST7735_BLACK);
 }
 
 void loop() {
+  int hall_val = digitalRead(hall_pin);
+  if (hall_state != hall_val && hall_val == LOW) {
+    revolutions++;
+    last_interval = millis() - last_fall;
+    last_fall = millis();
+  }
+  hall_state = hall_val;
+  updateSpeedAndDistance();
+
+  Serial.print("Hall:" );
+  Serial.print(hall_state);
+  Serial.print("  ");
+  Serial.print("Mph:");
+  Serial.print(mph);
+  Serial.print("  ");
+  Serial.print("Miles:");
+  Serial.println(distance);
+
+  tft.setTextSize(2);
   tft.setCursor(0, 0);
   tft.setTextColor(ST7735_YELLOW);
-  tft.setTextSize(2);
-  tft.println("  Road Bike");
-  tft.setCursor(0, 30);
-  tft.setTextColor(ST7735_WHITE);
-  tft.setTextSize(1);
-  tft.println("Speed = ");
+  tft.print("  Road Bike");
+
+  unsigned long currentMillis = millis();
+  if (currentMillis - previousMillis >= interval) {
+    previousMillis = currentMillis;
+    
+    kmh = mph/0.62137;
+    tft.setTextColor(ST7735_BLACK);    
+    tft.setCursor(0, 20);
+    tft.print("Kmh = ");
+    tft.println(pev_kmh);
+    tft.print("Mph = ");
+    tft.println(pev_mph);
+    tft.print("Miles = ");
+    tft.print(pev_distance);
+
+    tft.setTextColor(ST7735_WHITE);
+    tft.setCursor(0, 20);
+    tft.print("Kmh = ");
+    tft.println(kmh);
+    tft.print("Mph = ");
+    tft.println(mph);
+    tft.print("Miles = ");
+    tft.print(distance);
+    
+    pev_kmh = kmh;
+    pev_mph = mph;
+    pev_distance = distance;
+  }
+
+
 }
 
+/*************** Update Speed And Distance **************************/
+void updateSpeedAndDistance() {
+  distance = tire_circumference * revolutions;
+  distance /= miles_in_inches;
+  float ipm = tire_circumference / (float)last_interval;
+  mph = ipm * k_ipm_2_mph;
+}
+/*********************************************************************/
+/************************ bmpDraw ************************************/
 #define BUFFPIXEL 20
 void bmpDraw(char *filename, uint8_t x, uint8_t y) {
 
@@ -189,3 +283,4 @@ uint32_t read32(File f) {
   ((uint8_t *)&result)[3] = f.read(); // MSB
   return result;
 }
+/*********************************************************************/
